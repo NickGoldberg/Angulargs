77a145a1d4d1a9fdf3a62a548381eb9c1471e47d
Nick Goldberg <nick@goldberg.ws>
2017-06-04 22:09:47 -0400

    first commit

---
+++ src/app/smart-table-test/smart-table-test.component.ts
@@ -0,0 +1,49 @@
+﻿import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-smart-table-test',
+  styles: [],
+  template: `
+    <ng2-smart-table [settings]="settings" [source]="data"></ng2-smart-table>
+  `
+})
+export class SmartTableTestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  settings = {
+      columns: {
+          id: {
+              title: 'ID'
+          },
+          name: {
+              title: 'Full Name'
+          },
+          username: {
+              title: 'User Name'
+          },
+          email: {
+              title: 'Email'
+          }
+      }
+  };
+
+  data = [
+      {
+          id: 1,
+          name: "Leanne Graham",
+          username: "Bret",
+          email: "Sincere@april.biz"
+      },
+      // ... other rows here
+      {
+          id: 11,
+          name: "Nicholas DuBuque",
+          username: "Nicholas.Stanton",
+          email: "Rey.Padberg@rosamond.biz"
+      }
+  ];
+}
